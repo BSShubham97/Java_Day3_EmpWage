56ccffd77cf990e6ae2a5510545d96bfe1288df8
Shubham Bandagale <75619081+BSShubham97@users.noreply.github.com>
2021-08-08 23:25:02 +0530

    Use case 6

---
+++ EmpWageUC6.java
@@ -0,0 +1,41 @@
+public class EmpWageUC6 {
+    public static void main(String[] args) {
+        System.out.println("WAGE PER HOUR IS 20 & MAXIMUM WORK HOURS IS 8 & MAX WORK DAYS ARE 20 ");
+
+        int i ;
+        int emp_check;
+        int emp_work_time;
+        int total_work_time=0;
+
+
+            for (i = 1; i <= 20; i++)//Repeating the loop for 20 days
+            {
+                if (total_work_time <= 100 ) //working hours should not exceed 100hrs
+                {
+                System.out.println("DAY:" + i);
+                emp_check = (int) (Math.random() * 10) % 2;
+                int attend = emp_check;
+
+                switch (attend) {
+                    case 1:
+                        emp_work_time = (int) (Math.random() * 10) % 9;
+                        total_work_time = total_work_time + emp_work_time;
+                        System.out.println("This days work time: " + emp_work_time);
+                        break;
+                    case 0:
+                        emp_work_time = 0;
+                        total_work_time = total_work_time + emp_work_time;
+                        System.out.println("This days work time: " + emp_work_time);
+                        break;
+
+                }
+            }
+        }
+
+        System.out.println("Total working hours: "+total_work_time+"hrs");
+        int total_pay = total_work_time * 20;
+
+
+        System.out.println("Total wage of the Employee: " + total_pay);
+    }
+}
